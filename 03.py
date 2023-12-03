f18697af7a705486681f9e4916d77c2bb043a7f7
Michael Fogleman <fogleman@gmail.com>
2023-12-03 17:58:35 -0500

    day 3

--- 03.py
+++ 03.py
@@ -1,55 +1,31 @@
-from collections import *
-from itertools import *
+from collections import defaultdict
 import fileinput
-import math
 import re
 
-lines = list(fileinput.input())
-
-grid = []
-for line in lines:
-    grid.append(line.rstrip())
-
+numbers = []
 symbols = set()
 gears = set()
-for y, row in enumerate(grid):
-    for x, c in enumerate(row):
-        if c not in '.0123456789':
-            symbols.add((x, y))
-        if c == '*':
+for y, row in enumerate(fileinput.input()):
+    for m in re.finditer(r'\d+', row):
+        x0, x1 = m.span()
+        numbers.append((x0, x1, y, int(m.group())))
+    for m in re.finditer(r'[^\d\.\n]', row):
+        x = m.span()[0]
+        symbols.add((x, y))
+        if m.group() == '*':
             gears.add((x, y))
 
-total = 0
-for y, row in enumerate(grid):
-    for m in re.finditer(r'\d+', row):
-        x0, x1 = m.span()
-        ok = False
-        for x in range(x0, x1):
-            for sx, sy in symbols:
-                dx = abs(sx-x)
-                dy = abs(sy-y)
-                if dx <= 1 and dy <= 1:
-                    ok = True
-        if ok:
-            total += int(m.group())
-print(total)
+def adjacent(x0, x1, y, symbols):
+    return {(sx, sy) for x in range(x0, x1) for sx, sy in symbols
+        if abs(sx - x) <= 1 and abs(sy - y) <= 1}
 
-total = 0
-gear_ratios = defaultdict(list)
-for y, row in enumerate(grid):
-    for m in re.finditer(r'\d+', row):
-        x0, x1 = m.span()
-        ok = False
-        for x in range(x0, x1):
-            for sx, sy in gears:
-                dx = abs(sx-x)
-                dy = abs(sy-y)
-                if dx <= 1 and dy <= 1:
-                    ok = (sx, sy)
-        if ok:
-            gear_ratios[ok].append(int(m.group()))
-total = 0
-for gears in gear_ratios.values():
-    if len(gears) == 2:
-        total += gears[0] * gears[1]
-print(total)
+# part 1
+print(sum(number for x0, x1, y, number in numbers
+    if adjacent(x0, x1, y, symbols)))
+
+# part 2
+gear_numbers = defaultdict(list)
+for x0, x1, y, number in numbers:
+    for k in adjacent(x0, x1, y, gears):
+        gear_numbers[k].append(number)
+print(sum(g[0] * g[1] for g in gear_numbers.values() if len(g) == 2))
