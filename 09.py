488f36ded166d1452730f61102afa336ceb6dfc8
Michael Fogleman <fogleman@gmail.com>
2023-12-09 13:27:57 -0500

    day 9

--- 09.py
+++ 09.py
@@ -1,30 +1,21 @@
-from itertools import *
-from collections import *
-from math import *
 import fileinput
 import re
 
 lines = list(fileinput.input())
 
-def extrapolate(values, depth):
-    d = [b - a for a, b in zip(values, values[1:])]
-    if not any(d):
-        result = values[-1]
-    else:
-        result = values[-1] + extrapolate(d, depth+1)
+def extrapolate(values, index, sign):
+    diff = [b - a for a, b in zip(values, values[1:])]
+    result = values[index]
+    if any(diff):
+        result += extrapolate(diff, index, sign) * sign
     return result
 
-def extrapolate(values, depth):
-    d = [b - a for a, b in zip(values, values[1:])]
-    if not any(d):
-        result = values[0]
-    else:
-        result = values[0] - extrapolate(d, depth+1)
-    return result
+def run(index, sign):
+    total = 0
+    for line in lines:
+        values = list(map(int, line.split()))
+        total += extrapolate(values, index, sign)
+    return total
 
-total = 0
-for line in lines:
-    values = list(map(int, line.strip().split()))
-    print(values, extrapolate(values, 0))
-    total += extrapolate(values, 0)
-print(total)
+print(run(-1, 1))
+print(run(0, -1))
