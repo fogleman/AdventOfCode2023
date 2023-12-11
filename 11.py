a260d3f2d28ef6f1f44145ddf7cafdfa4e24c5e1
Michael Fogleman <fogleman@gmail.com>
2023-12-11 18:51:48 -0500

    day 11

--- 11.py
+++ 11.py
@@ -1,53 +1,19 @@
 import fileinput
 
-grid = []
-for line in fileinput.input():
-    grid.append(line.rstrip())
+grid = [line.rstrip() for line in fileinput.input()]
 
-def empty_y(grid):
-    h = len(grid)
-    w = len(grid[0])
-    result = set()
-    for y in range(h):
-        row = [grid[y][x] for x in range(w)]
-        if all(c == '.' for c in row):
-            result.add(y)
-    return result
+w, h = len(grid[0]), len(grid)
+xs = set(x for x in range(w) if all(grid[y][x] == '.' for y in range(h)))
+ys = set(y for y in range(h) if all(grid[y][x] == '.' for x in range(w)))
+positions = [(x, y) for y in range(h) for x in range(w) if grid[y][x] == '#']
 
-def empty_x(grid):
-    h = len(grid)
-    w = len(grid[0])
-    result = set()
-    for x in range(w):
-        col = [grid[y][x] for y in range(h)]
-        if all(c == '.' for c in col):
-            result.add(x)
-    return result
-
-def find_galaxies(grid):
-    h = len(grid)
-    w = len(grid[0])
-    result = []
-    for y in range(h):
-        for x in range(w):
-            if grid[y][x] == '#':
-                result.append((x, y))
-    return result
-
-positions = find_galaxies(grid)
-X = empty_x(grid)
-Y = empty_y(grid)
-total = 0
-for i, (x0, y0) in enumerate(positions):
-    for x1, y1 in positions[i+1:]:
-        X0, X1 = min(x0, x1), max(x0, x1)
-        Y0, Y1 = min(y0, y1), max(y0, y1)
-        d = abs(x1-x0) + abs(y1-y0)
-        for x in range(X0, X1+1):
-            if x in X:
-                d += 1000000-1
-        for y in range(Y0, Y1+1):
-            if y in Y:
-                d += 1000000-1
-        total += d
-print(total)
+for e in [2, 1000000]:
+    total = 0
+    for i, (x0, y0) in enumerate(positions):
+        for x1, y1 in positions[i+1:]:
+            X0, X1 = min(x0, x1), max(x0, x1)
+            Y0, Y1 = min(y0, y1), max(y0, y1)
+            total += X1 - X0 + Y1 - Y0
+            total += sum(x in xs for x in range(X0, X1)) * (e - 1)
+            total += sum(y in ys for y in range(Y0, Y1)) * (e - 1)
+    print(total)
